0c5d2195c062084ad4d053d28c38a3f4672162f0
namlt3820 <namlt3820@gmail.com>
2019-08-06 13:59:27 +0700

    feat: date-range-picker without styling

--- src/components/core/DateRangePicker.vue
+++ src/components/core/DateRangePicker.vue
@@ -1,17 +1,21 @@
 <template>
-    <date-picker
-        v-model="range"
-        lang="en"
-        range
-        type="date"
-        format="YYYY-MM-DD"
-        placeholder="Select Date"
-        width="500"
-        confirm
-        editable="'false'"
-    >
-        ></date-picker
-    >
+    <div>
+        <date-picker
+            v-model="range"
+            :lang="lang"
+            range
+            type="date"
+            format="Ngày DD/MM/YYYY"
+            placeholder="Chọn thời gian"
+            width="500"
+            confirm
+            :editable="false"
+            :shortcuts="shortcuts"
+            range-separator=" ~ "
+        >
+            ></date-picker
+        >
+    </div>
 </template>
 
 <script>
@@ -23,7 +27,71 @@ export default {
     },
     data() {
         return {
-            range: ''
+            range: '',
+            lang: {
+                days: ['CN', 'Hai', 'Ba', 'Bốn', 'Năm', 'Sáu', 'Bảy'],
+                months: [
+                    'Tháng 1',
+                    'Tháng 2',
+                    'Tháng 3',
+                    'Tháng 4',
+                    'Tháng 5',
+                    'Tháng 6',
+                    'Tháng 7',
+                    'Tháng 8',
+                    'Tháng 9',
+                    'Tháng 10',
+                    'Tháng 11',
+                    'Tháng 12'
+                ]
+            },
+            shortcuts: [
+                {
+                    text: 'Hôm nay',
+                    onClick: () => {
+                        this.range = [new Date(), new Date()];
+                    }
+                },
+                {
+                    text: 'Tuần này',
+                    onClick: () => {
+                        const date = new Date();
+                        const firstDay = new Date(date.setDate(date.getDate() - date.getDay() + 1));
+                        const lastDay = new Date(date.setDate(date.getDate() - date.getDay() + 7));
+                        this.range = [firstDay, lastDay];
+                    }
+                },
+                {
+                    text: 'Tháng này',
+                    onClick: () => {
+                        const date = new Date();
+                        const year = date.getFullYear();
+                        const month = date.getMonth();
+                        const firstDay = new Date(year, month, 1);
+                        const lastDay = new Date(year, month + 1, 0);
+                        this.range = [firstDay, lastDay];
+                    }
+                },
+                {
+                    text: '30 Ngày trước',
+                    onClick: () => {
+                        const today = new Date();
+                        const last30Days = new Date(today.setDate(today.getDate() - 30));
+                        this.range = [last30Days, new Date()];
+                    }
+                },
+                {
+                    text: 'Năm này',
+                    onClick: () => {
+                        const date = new Date();
+                        const year = date.getFullYear();
+                        const firstDay = new Date(year, 0, 1);
+                        const lastDay = new Date(year, 11, 31);
+                        this.range = [firstDay, lastDay];
+                    }
+                }
+            ],
+            mode: ['Hôm nay', 'Tuần này', 'Tháng này', '30 ngày trước', 'Năm này', 'Tuỳ chọn']
         };
     }
 };
--- src/views/customer/Lists.vue
+++ src/views/customer/Lists.vue
@@ -1,30 +1,106 @@
 <template>
-    <v-card>
-        <v-card-text>
-            <v-card class="title-grid" color="primary">
-                <v-card-title>
-                    <div>
-                        <h3 class="headline t1">Simple Table</h3>
-                        <div class="t2">Here is a subtitle for this table</div>
-                    </div>
-                    <div>
-                        <v-btn color="success" text>Add Customer</v-btn>
-                    </div>
-                </v-card-title>
-            </v-card>
-        </v-card-text>
-    </v-card>
+    <v-layout column px-5>
+        <v-flex style="border: 1px solid black">
+            <h2>Danh sách đối tác</h2>
+            <div class="text-right">
+                <div class="d-inline-block mr-3">
+                    <v-btn right class="blue white--text">Tạo mới</v-btn>
+                </div>
+            </div>
+            <v-layout justify-center>
+                <v-flex xs10 sm8 md6>
+                    <v-form>
+                        <v-layout align-baseline wrap>
+                            <v-text-field
+                                placeholder="Nhập tên đối tác"
+                                outlined
+                                prepend-inner-icon="mdi-magnify"
+                            ></v-text-field>
+                            <v-btn class="blue white--text ml-sm-4">Tìm kiếm</v-btn>
+                        </v-layout>
+                        <v-layout justify-left wrap>
+                            <v-flex xs10 sm8 md6 px-0
+                                ><v-select :items="filter" label="Lọc" outlined></v-select
+                            ></v-flex>
+                        </v-layout>
+                    </v-form>
+                </v-flex>
+            </v-layout>
+            <v-simple-table>
+                <thead class="brown lighten-3">
+                    <tr>
+                        <th class="text-left white--text">STT</th>
+                        <th
+                            class="text-left white--text"
+                            v-for="(item, index) in tableData[0]"
+                            :key="index"
+                        >
+                            {{ item.name }}
+                        </th>
+                        <th class="text-left white--text">Hành động</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="position in 7" :key="position">
+                        <td>{{ position }}</td>
+                        <td v-for="(item, index) in tableData[0]" :key="index">{{ item.value }}</td>
+                        <td>
+                            <v-icon>mdi-eye</v-icon>&nbsp; <v-icon>mdi-content-copy</v-icon>&nbsp;
+                            <v-icon>mdi-square-edit-outline</v-icon>
+                        </td>
+                    </tr>
+                </tbody>
+            </v-simple-table>
+            <custom-pagination></custom-pagination>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
-// @ is an alias to /src
+import CustomPagination from '../../components/core/Pagination';
 
 export default {
     name: 'Lists',
     data() {
-        return {};
+        return {
+            filter: ['Không', 'PO', 'Merchant', 'Trạng thái'],
+            tableData: [
+                [
+                    {
+                        name: 'Tên đối tác',
+                        value: 'Vpoint'
+                    },
+                    {
+                        name: 'Loại đối tác',
+                        value: 'PO'
+                    },
+                    {
+                        name: 'Loại điểm',
+                        value: 'Vpoint'
+                    },
+                    {
+                        name: 'Giá trị điểm tương ứng (số tiền)',
+                        value: '1 Vpoint=1.000 VND'
+                    },
+                    {
+                        name: 'Giá trị điểm tương ứng (điểm Ipoint)',
+                        value: '1 Vpoint= 1 Ipoint'
+                    },
+                    {
+                        name: 'Thời gian tạo',
+                        value: '01/01/2019'
+                    },
+                    {
+                        name: 'Trạng thái',
+                        value: 'Active'
+                    }
+                ]
+            ]
+        };
     },
     created() {},
-    components: {}
+    components: {
+        CustomPagination
+    }
 };
 </script>
